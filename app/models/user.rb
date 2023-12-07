92426a2ae0cc74aa00cde1ad141cb68004039541
ts-0105 <0a89z0@gmail.com>
2023-12-07 11:08:59 +0900

    バリデーションに正規表現で制限を設けた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  
+  with_options presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}a-zA-Z]+\z/, message: "は漢字、ひらがな、カタカナ、英語のみを含めることができます" } do
+  validates :lastname
+  validates :firstname
+        end
+
+  with_options presence: true, format: { with: /\A[\p{Katakana}ー－]+\z/, message: "はカタカナのみを含めることができます" } do
+  validates :lastname_kana, presence: true
+  validates :firstname_kana, presence: true
+        end
+
+  validates :birthday, presence: true
+
 end
