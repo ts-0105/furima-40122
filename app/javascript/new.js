0bfeec1651c0317c1eaf28a88e2817cf3d5417a9
ts-0105 <0a89z0@gmail.com>
2023-12-19 19:44:30 +0900

    javascruptのNaN問題解消

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -2,15 +2,21 @@ function calculation() {
   const calculate = document.getElementById("item-price");
 
   calculate.addEventListener("input", function () {
-    let input = parseFloat(calculate.value); // 入力された値を取得
+    let input = calculate.value.trim(); // 入力された値を取得し、前後の空白を削除
+    if (input === "") {
+      input = 0; // もし空の場合、デフォルト値として0を設定する
+    } else {
+      input = parseFloat(input); // 入力が空でない場合、数値に変換
+    }
+
     let tenPercent = Math.floor(input * 0.1); // 入力された値の10%を計算
-    let commission = document.getElementById("add-tax-price"); //入力された数値の10%を表示
+    let commission = document.getElementById("add-tax-price");
     commission.innerHTML = tenPercent;
-    let profit = document.getElementById("profit"); //入力された数値から10%引いて小数点を繰り上げた数値表示
+
+    let profit = document.getElementById("profit");
     profit.innerHTML = input - tenPercent;
   });
 }
 
 window.addEventListener("turbo:load", calculation);
 window.addEventListener("turbo:render", calculation);
-
