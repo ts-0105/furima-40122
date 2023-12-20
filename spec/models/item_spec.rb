7041f978e1734f98b0806be456dbfade9b22718c
ts-0105 <0a89z0@gmail.com>
2023-12-20 11:53:27 +0900

    一覧表示、itemの情報表示、if条件表示

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -106,7 +106,7 @@ RSpec.describe Item, type: :model do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
-        end
+      end
     end
   end
 end
