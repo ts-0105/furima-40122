809109dced970cd14fc9811ce53da92c20bd6e3b
ts-0105 <0a89z0@gmail.com>
2023-12-21 14:19:48 +0900

    条件文変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return unless user_signed_in? && current_user.id != @item.user.id
+    if user_signed_in? && current_user.id != @item.user.id
     redirect_to action: :index
   end
+end
 end
